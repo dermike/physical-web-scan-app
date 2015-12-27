405279fd0a80afa92fedf099657b852b5b936334
Mikael Jergefelt <mikael.jergefelt@ki.se>
2015-12-27 14:09:04 +0100

    lint and use ipcRenderer instead of deprecated ipc

--- js/app.js
+++ js/app.js
@@ -1,32 +1,31 @@
-(function() {
-  var ipc = require('ipc'),
-      shell = require('shell'),
-      results = document.getElementById('results'),
-      status = document.getElementById('status');
+(function pw() {
+  var ipc = require('electron').ipcRenderer,
+    shell = require('shell'),
+    results = document.getElementById('results'),
+    status = document.getElementById('status');
 
-  ipc.on('status', function(message, clear) {
+  function go(link) {
+    shell.openExternal(link);
+  }
+
+  ipc.on('status', function ipcstatus(event, message, clear) {
     status.innerHTML = message;
     if (clear) {
       results.innerHTML = '';
     }
   });
 
-  ipc.on('url', function(message) {
+  ipc.on('url', function ipcurl(event, message) {
     var button = document.createElement('button'),
-        desc = document.createElement('p');
-    
+      desc = document.createElement('p');
+
     button.innerHTML = message[0];
-    button.onclick = function() { go(message[2]); };
-    
+    button.onclick = function bclick() { go(message[2]); };
+
     desc.innerHTML = '<a href="#">' + message[2] + '</a>' + message[1];
-    desc.onclick = function() { go(message[2]); };
-    
+    desc.onclick = function dclick() { go(message[2]); };
+
     results.insertBefore(desc, results.firstChild);
     results.insertBefore(button, results.firstChild);
   });
-
-  function go(link) {
-    shell.openExternal(link);
-  }
-  
 })();
