840141032e91f9e4a45f53f2d525879fe30c9e6a
Mikael Jergefelt <mikael.jergefelt@ki.se>
2017-03-30 19:41:09 +0200

    use promise instead of callback for metadata

--- main.js
+++ main.js
@@ -36,7 +36,7 @@ noble.on('discover', peripheral => {
       }
     }
     if (objects.length) {
-      metadata(objects, message => {
+      metadata(objects).then((message) => {
         mainWindow.webContents.send('url', message);
         if (!mainWindow.isFocused()) {
           counter += 1;
